7c58dff76e45e93334853a31cc7818d292de0b89
sherry <1570053050@qq.com>
2023-04-21 17:34:58 +0800

    背景修改

--- src/.vuepress/plugins/vuepress-plugin-background/canvas/square.ts
+++ src/.vuepress/plugins/vuepress-plugin-background/canvas/square.ts
@@ -4,10 +4,23 @@ function square() {
     const ul = document.createElement("ul");
     for(let i:number=1;i<=6;i++){
         const li=document.createElement("li")
+        const span1=document.createElement("span")
+        const span2=document.createElement("span")
+        const span3=document.createElement("span")
+        const span4=document.createElement("span")
+        span1.className = "span1"
+        span2.className = "span2"
+        span3.className = "span3"
+        span4.className = "span4"
+        li.appendChild(span1)
+        li.appendChild(span2)
+        li.appendChild(span3)
+        li.appendChild(span4)
         ul.appendChild(li)
     }
-    div.appendChild(ul)
     div.className = "square-bg";
+    ul.className = "square-ul"
     document.body.appendChild(div);
+    document.body.appendChild(ul)
 }
 export default square;
