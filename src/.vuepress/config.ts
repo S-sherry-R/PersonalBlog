490ad986d1d2ef64e7fd8cef3eb1bd4320cba217
sherry <1570053050@qq.com>
2023-06-21 14:12:04 +0800

    更新background

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -33,7 +33,6 @@ export default defineUserConfig({
   },
 
   theme,
-
   port: 9527,
 
   plugins: [
@@ -53,7 +52,12 @@ export default defineUserConfig({
     live2DAssistPlugin({
       subPageHidden: true,
     }),
-    backPlugin(),
+    backPlugin({
+      type: 'normalLine',
+      options: {
+        backColor: '#c1e8eb'
+      }
+    }),
     // 谷歌统计
     googleAnalyticsPlugin({
       // 配置项
@@ -61,54 +65,54 @@ export default defineUserConfig({
       debug: true,
     }),
     // 搜索插件
-    docsearchPlugin({
-      appId: "25GFQHJ3GQ",
-      apiKey: "c825d29420633e105fe480746cee7512",
-      indexName: "sherrySR",
-      locales: {
-        "/": {
-          placeholder: "搜索内容",
-          translations: {
-            button: {
-              buttonText: "搜索",
-              buttonAriaLabel: "搜索",
-            },
-            modal: {
-              searchBox: {
-                resetButtonTitle: "清除查询条件",
-                resetButtonAriaLabel: "清除查询条件",
-                cancelButtonText: "取消",
-                cancelButtonAriaLabel: "取消",
-              },
-              startScreen: {
-                recentSearchesTitle: "搜索历史",
-                noRecentSearchesText: "没有搜索历史",
-                saveRecentSearchButtonTitle: "保存至搜索历史",
-                removeRecentSearchButtonTitle: "从搜索历史中移除",
-                favoriteSearchesTitle: "收藏",
-                removeFavoriteSearchButtonTitle: "从收藏中移除",
-              },
-              errorScreen: {
-                titleText: "无法获取结果",
-                helpText: "你可能需要检查你的网络连接",
-              },
-              footer: {
-                selectText: "选择",
-                navigateText: "切换",
-                closeText: "关闭",
-                searchByText: "搜索提供者",
-              },
-              noResultsScreen: {
-                noResultsText: "无法找到相关结果",
-                suggestedQueryText: "你可以尝试查询",
-                reportMissingResultsText: "你认为该查询应该有结果？",
-                reportMissingResultsLinkText: "点击反馈",
-              },
-            },
-          },
-        },
-      },
-    }),
+    // docsearchPlugin({
+    //   appId: "25GFQHJ3GQ",
+    //   apiKey: "c825d29420633e105fe480746cee7512",
+    //   indexName: "sherrySR",
+    //   locales: {
+    //     "/": {
+    //       placeholder: "搜索内容",
+    //       translations: {
+    //         button: {
+    //           buttonText: "搜索",
+    //           buttonAriaLabel: "搜索",
+    //         },
+    //         modal: {
+    //           searchBox: {
+    //             resetButtonTitle: "清除查询条件",
+    //             resetButtonAriaLabel: "清除查询条件",
+    //             cancelButtonText: "取消",
+    //             cancelButtonAriaLabel: "取消",
+    //           },
+    //           startScreen: {
+    //             recentSearchesTitle: "搜索历史",
+    //             noRecentSearchesText: "没有搜索历史",
+    //             saveRecentSearchButtonTitle: "保存至搜索历史",
+    //             removeRecentSearchButtonTitle: "从搜索历史中移除",
+    //             favoriteSearchesTitle: "收藏",
+    //             removeFavoriteSearchButtonTitle: "从收藏中移除",
+    //           },
+    //           errorScreen: {
+    //             titleText: "无法获取结果",
+    //             helpText: "你可能需要检查你的网络连接",
+    //           },
+    //           footer: {
+    //             selectText: "选择",
+    //             navigateText: "切换",
+    //             closeText: "关闭",
+    //             searchByText: "搜索提供者",
+    //           },
+    //           noResultsScreen: {
+    //             noResultsText: "无法找到相关结果",
+    //             suggestedQueryText: "你可以尝试查询",
+    //             reportMissingResultsText: "你认为该查询应该有结果？",
+    //             reportMissingResultsLinkText: "点击反馈",
+    //           },
+    //         },
+    //       },
+    //     },
+    //   },
+    // }),
     // 看板娘插件
     ohmylive2dPlugin({}),
   ],
